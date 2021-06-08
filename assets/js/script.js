d393faae021c0ed56a9315d725c885a815d7390c
Nicolas Rafael Duma <nicolasrafaelduma@outlook.com>
2021-06-08 17:11:24 -0300

    building bord

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 const front = "card-front"
-const back = "back"
+const back = "card-back"
 
 const technologies = [
     "bootstrap",
@@ -21,6 +21,48 @@ startGame()
 function startGame() {
     cards = createCardsFromTecnologies()
     shuffleCards(cards)
+
+    initializeBord(cards)
+}
+
+function initializeBord(cards) {
+    let gameBord = document.getElementById("game-bord")
+
+    for (let card of cards) {
+        let cardElement = document.createElement("div")
+        cardElement.id = card.id
+        cardElement.classList.add("card")
+        cardElement.dataset.icon = card.icon
+
+        createCardContent(card, cardElement)
+
+        cardElement.onclick = flipCard
+
+        gameBord.appendChild(cardElement)
+    }
+}
+
+function createCardContent(card, cardElement) {
+    createCardFace(front, card, cardElement)
+    createCardFace(back, card, cardElement)
+}
+
+function createCardFace(face, card, element) {
+    let cardElementFace = document.createElement("div")
+    cardElementFace.classList.add(face)
+
+    if (face === front) {
+        let iconElement = document.createElement("img")
+        iconElement.classList.add("icon")
+        iconElement.src = "./assets/images/" + card.icon + ".png"
+        iconElement.alt = "logo " + card.icon
+
+        cardElementFace.appendChild(iconElement)
+    } else if (face === back) {
+        cardElementFace.innerHTML = "&lt;/&gt;"
+    }
+
+    element.appendChild(cardElementFace)
 }
 
 function createCardsFromTecnologies() {
@@ -55,4 +97,8 @@ function shuffleCards(cards) {
 
         [cards[randomIndex], cards[currentIndex]] = [cards[currentIndex], cards[randomIndex]]
     }
+}
+
+function flipCard() {
+    this.classList.add("flip")
 }
